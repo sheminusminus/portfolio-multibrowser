812c671f66c5d4467ffea2fb3f0b5a70ab95b75e
Emily Kolar <emkolardev@gmail.com>
2018-06-06 20:36:43 -0500

    add portfolio repo link

--- src/components/Footer/presenter.jsx
+++ src/components/Footer/presenter.jsx
@@ -6,8 +6,15 @@ class Footer extends Component {
   render() {
     return (
       <div className={styles.footer}>
-        <p>{String.fromCharCode(9829)} made in chicago</p>
-        <p>{String.fromCharCode(169)} emily kolar, 2018</p>
+        <div className={styles.repo}>
+          <a href="https://github.com/sheminusminus/portfolio-multibrowser" target="_blank">
+            the repo for this site
+          </a>
+        </div>
+        <div className={styles.typical}>
+          <p>{String.fromCharCode(9829)} made in chicago</p>
+          <p>{String.fromCharCode(169)} emily kolar, 2018</p>
+        </div>
       </div>
     );
   }
